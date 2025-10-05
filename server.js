2c809a6aea34ceb0001e7253442a7a91c7977d48
Nitesh Kumar <nitesh565golu@gmail.com>
2025-10-05 17:44:52 +0530

    lastone

--- server.js
+++ server.js
@@ -10,16 +10,18 @@ import reviewRoutes from './routes/reviewRoutes.js';
 
 dotenv.config();
 
-// const corsOptions = {
-//   origin: process.env.FRONTEND_URL || "http://localhost:5173",
-//   credentials: true,
-// };
+
 
 const app = express();
 app.use(express.json());
 app.use(morgan("dev"));
 
-app.use(cors());
+const corsOptions = {
+  origin:"http://localhost:5173",
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 
 //routes
 app.use("/api", authRoutes);
